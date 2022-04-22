3d424a59224bc459ecfe369924a7250cdc607f30
tahrer007 <tahrer_Ad@outlook.com>
2022-04-23 01:47:10 +0300

    make nested navgation work with buttons

--- mobile-client/src/components/UserAuthentication/userProfile.jsx
+++ mobile-client/src/components/UserAuthentication/userProfile.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React ,{ useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ReusableButton from "../reusabeles/Button";
+
+
 //if the user logged in it will show log out button and user details
 //other wise will show log in /sign up / sign up by google
-const UserProfile = () => {
-  const OnPress = (title) => console.log(title);
+const UserProfile = ({navigation}) => {
+  useEffect(() => {
+  //navigation.navigate("ByGoogle") ;
+  }, []);
+
+  const OnPress = (routeName) => navigation.navigate(routeName);
   return (
     <View style={styles.container}>
-      <ReusableButton onPress={OnPress} title="Log In" />
-      <ReusableButton onPress={OnPress} title="Sign Up" />
-      <ReusableButton onPress={OnPress} title="Sign up By Google" />
+      <ReusableButton onPress={OnPress} title="Log In" RouteName={"SignIn"} />
+      <ReusableButton onPress={OnPress} title="Sign Up" RouteName={"signup"} />
+      <ReusableButton
+        onPress={OnPress}
+        title="Sign up By Google"
+        RouteName={"ByGoogle"}
+      />
     </View>
   );
 };
--- mobile-client/src/components/reusabeles/Button.jsx
+++ mobile-client/src/components/reusabeles/Button.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Button, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-const ReusableButton = ({ title, onPress }) => {
+const ReusableButton = ({ title, onPress ,RouteName}) => {
   return (
     <TouchableOpacity style={styles.Button}>
       <Button
-        onPress={() => onPress(title)}
+        onPress={() => onPress(RouteName)}
         title={title}
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
