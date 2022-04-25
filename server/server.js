ec413dd5d3fb08458e3e9410c43a87a39c52db9c
tahrer007 <tahrer_Ad@outlook.com>
2022-04-26 00:02:50 +0300

    add dotenv

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+require("dotenv").config();
+require("./dataBase/mongoose");
+
+const app = express();
 
 //routers
 const locationsRouter = require("./routers/locationsRouter");
