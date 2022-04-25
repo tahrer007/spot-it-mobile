669aa0d9702b26baa46e167438e506fa6f0a3d4e
tahrer007 <tahrer_Ad@outlook.com>
2022-04-25 23:31:56 +0300

    nodemon

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+
+app.get("/help", (req, res) => {
+    res.send("help !!!! ");
+  });
 app.get("", (req, res) => {
   res.send("hellow express !!!! ");
 });
 
-app.get("/help", (req, res) => {
-  res.send("hellow express !!!! ");
-});
+
 app.listen(3000, () => {
   console.log("server is up on port 300.");
 });
