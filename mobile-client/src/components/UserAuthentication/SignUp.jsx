d1b7a47fb5c41586356bd720653f5563e28140dd
tahrer007 <tahrer_Ad@outlook.com>
2022-04-26 15:06:00 +0300

    add toggle buttons to login

--- mobile-client/src/components/UserAuthentication/SignUp.jsx
+++ mobile-client/src/components/UserAuthentication/SignUp.jsx
@@ -1,41 +1,37 @@
-import React  ,{useEffect , useState}from "react";
-import { View, Text ,StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import ReusableButton from "../reusabeles/Button";
-import InputField from "../reusabeles/InputField" ;
-import ToggleButton from "../reusabeles/ToggleButton";
- 
+import InputField from "../reusabeles/InputField";
 
 const SignUp = () => {
-  const [userName,setUserName]=useState("");
-  const [userEmail,setUserEmail]=useState("");
-  const [userPassword,setUserPassword]=useState("");
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
   const OnPress = (routeName) => console.log(routeName);
 
   return (
     <View style={styles.container}>
-     <InputField
+      <InputField
         placeholder="user name"
         secureTextEntry={false}
         value={userName}
         onChangeText={(userName) => setUserName(userName)}
       />
-       <InputField
+      <InputField
         placeholder="email"
         secureTextEntry={false}
         value={userEmail}
         onChangeText={(userEmail) => setUserEmail(userEmail)}
       />
-       <InputField
+      <InputField
         placeholder="password"
         secureTextEntry={true}
         value={userPassword}
         onChangeText={(userPassword) => setUserPassword(userPassword)}
       />
-      <ToggleButton label={"Remmber me"}/>
-      <ToggleButton label={"auto login"}/>
-    <ReusableButton onPress={OnPress} title="Sign Up" RouteName={"signup"} />
 
+      <ReusableButton onPress={OnPress} title="Sign Up" RouteName={"signup"} />
     </View>
   );
 };
