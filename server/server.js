1efc28392631d941286f9e69876c6972d20e565d
tahrer007 <tahrer_Ad@outlook.com>
2022-04-26 16:25:56 +0300

    create auth rout

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 require("./dataBase/mongoose");
 
+
 const app = express();
 
 //routers
 const locationsRouter = require("./routers/locationsRouter");
 const usersRouter = require("./routers/usersRouter");
 const keysRouter = require("./routers/keysRouter");
+const authRouter = require("./routers/authRouter") ;
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +19,7 @@ app.use(cors());
 app.use("/users", usersRouter);
 app.use("/locations", locationsRouter);
 app.use("/keys", keysRouter);
+app.use(authRouter) ; 
 
 app.use("*", (req, res) => {
   res.send("this route is not exist");
