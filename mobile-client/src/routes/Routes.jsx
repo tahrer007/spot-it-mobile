161b3359fd8cacff619c2951d7962526ea1126eb
tahrer007 <tahrer_Ad@outlook.com>
2022-04-27 23:26:20 +0300

    others

--- mobile-client/src/routes/Routes.jsx
+++ mobile-client/src/routes/Routes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "react-native-gesture-handler";
 import LoginScreen from "../screens/LoginScreen";
-import SignUpScreen from "../screens/SignUpScreen";
+import SignupScreen from "../screens/SignupScreen";
 import AboutScreen from "../screens/AboutScreen";
 import LocationsMap from "../screens/LocationsMap";
 import RedNeighborhoods from "../screens/RedNeighborhoods";
@@ -13,7 +13,7 @@ const Routes = () => {
   return (
     <Drawer.Navigator initialRouteName="Locations Map">
        <Drawer.Screen name="login" component={LoginScreen} />
-       <Drawer.Screen name="signup" component={SignUpScreen} />
+       <Drawer.Screen name="signup" component={SignupScreen} />
       <Drawer.Screen name="Locations Map" component={LocationsMap}  />
       <Drawer.Screen name="Red Neighborhoods" component={RedNeighborhoods} />
       <Drawer.Screen name="About" component={AboutScreen} />
