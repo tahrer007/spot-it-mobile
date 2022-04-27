3a1f49eebd1b400deb455cd7966a4074eef3bbcd
tahrer007 <tahrer_Ad@outlook.com>
2022-04-27 23:31:33 +0300

    update router

--- mobile-client/src/routes/Routes.jsx
+++ mobile-client/src/routes/Routes.jsx
@@ -4,6 +4,8 @@ import "react-native-gesture-handler";
 import LoginScreen from "../screens/LoginScreen";
 import SignupScreen from "../screens/SignupScreen";
 import AboutScreen from "../screens/AboutScreen";
+import AccountScreen from "../screens/AccountScreen";
+import SosScreen from "../screens/SosScreen";
 import LocationsMap from "../screens/LocationsMap";
 import RedNeighborhoods from "../screens/RedNeighborhoods";
 import { createDrawerNavigator } from "@react-navigation/drawer";
@@ -16,7 +18,8 @@ const Routes = () => {
        <Drawer.Screen name="signup" component={SignupScreen} />
       <Drawer.Screen name="Locations Map" component={LocationsMap}  />
       <Drawer.Screen name="Red Neighborhoods" component={RedNeighborhoods} />
-      <Drawer.Screen name="About" component={AboutScreen} />
+      <Drawer.Screen name="sos" component={SosScreen} />
+      <Drawer.Screen name="acount" component={AccountScreen} />
      
     </Drawer.Navigator>
   );
