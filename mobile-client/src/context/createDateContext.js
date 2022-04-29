c68fd886be6790131afb35f4867a66c1e036103c
tahrer007 <tahrer_Ad@outlook.com>
2022-04-30 02:14:12 +0300

    create global state by context

--- mobile-client/src/context/createDateContext.js
+++ mobile-client/src/context/createDateContext.js
@@ -1,3 +1,27 @@
 import React, { useReducer ,createContext } from "react";
-import { initialState } from "./reducers";
+import { initialState , authReducer } from "./reducers";
 export const store = createContext();
+
+const appProvider =({children}) =>{
+    const [state,dispatch]=useReducer(authReducer,initialState) ;
+
+    const signIn =(email,password)=>{
+        dispatch ({
+            type : "SIGN_IN",
+            payLoad : {
+
+            }
+        })
+
+    }
+    const logIn= () =>{
+
+    }
+    const logOut =()=>{
+
+    }
+    const value ={
+        isSignedIn : state.isSignedIn
+    }
+    return <store.appProvider>{children}</store.appProvider>
+}
