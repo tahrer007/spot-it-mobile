8e539865b3eb5411c7aec9ae63e96884783b8d37
tahrer007 <tahrer_Ad@outlook.com>
2022-04-30 03:25:04 +0300

    set up authcontext

--- mobile-client/src/context/AuthContext.js
+++ mobile-client/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDateContext from "./createDateContext";
+import api from "../services/api/api"
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -6,9 +7,24 @@ const authReducer = (state, action) => {
       return state;
   }
 };
+const signup= (dispatch)=>{
+  return ({email, password})=>{
+
+  }
+}
+const signin= (dispatch)=>{
+  return ({email, password})=>{
+    
+  }
+}
+const signout =(dispatch)=>{
+  return ()=>{
+
+  }
+}
 
 export const { Provider, Context } = createDateContext(
   authReducer,
-  {},//actions
+  {signup,signin,signout},//actions
   { isSingnedIn: false }//state 
 );
