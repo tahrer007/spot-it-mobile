d7e48813c430a67e5331268a48fb33dab4fadde2
tahrer007 <tahrer_Ad@outlook.com>
2022-05-01 18:25:23 +0300

    sign in screen and context

--- mobile-client/src/screens/LogInScreen.jsx
+++ mobile-client/src/screens/LogInScreen.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React ,{useContext,useState ,useEffect}from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import ToggleButton from "../components/reusabeles/ToggleButton";
+import ReusableButton from "../components/reusabeles/Button";
+
+import Linked from "../components/reusabeles/Linked";
+import InputField from "../components/reusabeles/InputField";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const LoginScreen = () => {
+  const { state, signin } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+  const OnPress = (routeName) => {
+    signin({ email, password });
+  };
   return (
     <View style={styles.container}>
-      <Text> sign in </Text>
+      <InputField
+        placeholder="email"
+        secureTextEntry={false}
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+      />
+      <InputField
+        placeholder="password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+      />
+
       <ToggleButton label={"Remmber me"} />
-      <ToggleButton label={"auto login"} />
+      <Linked
+        text="This is your first time? sign up instead"
+        routeName="signup"
+      />
+      <ReusableButton onPress={OnPress} title="Sign in" routeName={"signup"} />
+      {/*<ToggleButton label={"auto login"} />*/}
     </View>
   );
 };
@@ -17,6 +48,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  errrorMessage: {
+    color: "red",
+    fontSize: 18,
   },
 });
 
