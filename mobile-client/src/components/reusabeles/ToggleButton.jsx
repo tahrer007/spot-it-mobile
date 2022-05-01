b3d3b11d89c7ca6a1523319eeb5bf1e6056057d1
tahrer007 <tahrer_Ad@outlook.com>
2022-05-01 18:26:47 +0300

    change switch button flex

--- mobile-client/src/components/reusabeles/ToggleButton.jsx
+++ mobile-client/src/components/reusabeles/ToggleButton.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
-import { View, Switch, StyleSheet ,Text } from 'react-native';
+import React, { useState } from "react";
+import { View, Switch, StyleSheet, Text } from "react-native";
 
-const ToggleButton = ({label}) => {
+const ToggleButton = ({ label }) => {
   const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   return (
     <View style={styles.container}>
       <Text> {label}</Text>
       <Switch
-        trackColor={{ false: '#767577', true: '#841584' }}
-        thumbColor={isEnabled ? '#841584' : '#f4f3f4'}
+        trackColor={{ false: "#767577", true: "#841584" }}
+        thumbColor={isEnabled ? "#841584" : "#f4f3f4"}
         ios_backgroundColor="#3e3e3e"
         onValueChange={toggleSwitch}
         value={isEnabled}
-  
       />
     </View>
   );
@@ -22,11 +21,9 @@ const ToggleButton = ({label}) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection : "row",
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin : 2 
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
