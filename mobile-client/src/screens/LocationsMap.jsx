fd50c6d9372d3e8c2dd0af52b4b8fc2df3edfc9c
tahrer007 <tahrer_Ad@outlook.com>
2022-05-04 14:32:20 +0300

    page formate

--- mobile-client/src/screens/LocationsMap.jsx
+++ mobile-client/src/screens/LocationsMap.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-import { View, Text ,StyleSheet ,Dimensions } from "react-native";
-import MapView from 'react-native-maps';
-import Polygon from 'react-native-maps';
-import HaifaCoords from "../services/haifaCoords"
-const initialRegion={
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+import Polygon from "react-native-maps";
+import HaifaCoords from "../services/haifaCoords";
+const initialRegion = {
   latitude: 32.794241949530296,
-  longitude:  34.98972566204482,
+  longitude: 34.98972566204482,
   latitudeDelta: 0.0922,
   longitudeDelta: 0.0421,
-}
+};
 
 const LocationsMap = () => {
   return (
     <View style={styles.container}>
-      
-      <MapView style={styles.map}
-      region ={initialRegion}
-      >
-       
-      </MapView>
+      <MapView style={styles.map} region={initialRegion}></MapView>
     </View>
   );
 };
@@ -26,13 +21,13 @@ const LocationsMap = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 
