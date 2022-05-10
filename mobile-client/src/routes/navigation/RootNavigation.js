d31db4d5a6d03fd633cff6c310662b6cdff036b8
tahrer007 <tahrer_Ad@outlook.com>
2022-05-10 22:50:28 +0300

    navigation

--- mobile-client/src/routes/navigation/RootNavigation.js
+++ mobile-client/src/routes/navigation/RootNavigation.js
@@ -2,9 +2,8 @@ import { createNavigationContainerRef } from "@react-navigation/native";
 
 const navigationRef = createNavigationContainerRef();
 
-const navigate = (name) =>{
+const navigate = (name) =>
     ( navigationRef.isReady()) ? navigationRef.navigate(name) : console.log("error ref nav ");
 
-}
 
 export default navigate;
