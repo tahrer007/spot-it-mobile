aa042f4bc216b1d0cc393873e6492f3101574025
tahrer007 <tahrer_Ad@outlook.com>
2022-05-10 22:50:43 +0300

    .

--- mobile-client/src/routes/navigation/RootNavigation.js
+++ mobile-client/src/routes/navigation/RootNavigation.js
@@ -3,7 +3,8 @@ import { createNavigationContainerRef } from "@react-navigation/native";
 const navigationRef = createNavigationContainerRef();
 
 const navigate = (name) =>
-    ( navigationRef.isReady()) ? navigationRef.navigate(name) : console.log("error ref nav ");
-
+  navigationRef.isReady()
+    ? navigationRef.navigate(name)
+    : console.log("error ref nav ");
 
 export default navigate;
