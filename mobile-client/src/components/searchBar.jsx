c07c9f842ba2785fdf763e00ce4a697cfc59af37
tahrer007 <tahrer_Ad@outlook.com>
2022-05-15 23:00:30 +0300

    search bar basic component

---
+++ mobile-client/src/components/searchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View  } from "react-native";
+
+
+const searchBar =()=> {
+  
+  
+  return (
+    <View> <Text> SEARCH BAR </Text></View>
+  );
+}
+export default searchBar
