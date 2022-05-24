b94abec65d67e72f1df4d3cb01815ee1bf780cf0
tahrer007 <tahrer_Ad@outlook.com>
2022-05-24 23:44:15 +0300

    web socket

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("./config/express");
 mongoose = require("mongoose");
 
 //require("./dataBase/mongoose");
-//cron = require("node-cron");
 
 // Use env port or default
 const port = process.env.PORT || 5000;
@@ -27,108 +26,34 @@ server.listen(port, () => console.log(`Server now running on port ${port}!`));
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.auigb.mongodb.net/WildAnimalsTracker?retryWrites=true&w=majority`;
-mongoose.connect(DB_URL , {
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
-  
 });
 
 const connection = mongoose.connection;
 
 connection.once("open", () => {
   console.log("MongoDB database connected");
-
-  console.log("Setting change streams");
   const thoughtChangeStream = connection.collection("locations").watch();
-
+  console.log("Setting change streams");
   thoughtChangeStream.on("change", (change) => {
-    console.log(change.operationType) ;
     switch (change.operationType) {
       case "insert":
-        const location = {
+        console.log(change.fullDocument)
+        /*const location = {
+
           _id: change.fullDocument._id,
           lat: change.fullDocument.lat,
           lng: change.fullDocument.lng,
-          time : change.fullDocument.time,
-          number : change.fullDocument.number, 
-          comment : change.fullDocument.comment,
-        };
+          time: change.fullDocument.time,
+          number: change.fullDocument.number,
+          comment: change.fullDocument.comment,
+        };*/
 
-        console.log(location) ;
-
-        io.of("/socket").emit("newLocation", location);
+        io.of("/socket").emit("newLocation", change.fullDocument);
         break;
-
-      /*case "delete":
-        io.of("/api/socket").emit("deletedThought", change.documentKey._id);
-        break;*/
     }
   });
 });
 
-//schedule deletion of thoughts at midnight
-/*cron.schedule("0 0 0 * * *", async () => {
-  await connection.collection("thoughts").drop();
-
-  io.of("/api/socket").emit("thoughtsCleared");
-});*/
-
 connection.on("error", (error) => console.log("Error: " + error));
-
-
-
-
-
-/*const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-require("./dataBase/mongoose");
-
-//to handle incoming cody json
-const bodyParser = require("body-parser");
-const requireAuth = require("./middlewares/requireAuth");
-const app = express();
-app.use(cors());
-//web socket
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
-//routers
-const locationsRouter = require("./routers/locationsRouter");
-const usersRouter = require("./routers/usersRouter");
-const keysRouter = require("./routers/keysRouter");
-const authRouter = require("./routers/authRouter");
-
-const PORT = process.env.PORT || 5000;
-
-app.get("/chat", requireAuth, (req, res) => {
-  res.send(`your email ${res.user.email}`);
-});
-
-app.use(authRouter);
-app.use("/users", usersRouter);
-app.use("/locations", locationsRouter);
-app.use("/keys", keysRouter);
-
-app.use("*", (req, res) => {
-  res.send("this route is not exist");
-});
-
-io.on("connection", socket => {
-  console.log("a user connected :D");
-  socket.on("chat message", msg => {
-    console.log(msg);
-    io.emit("chat message", msg);
-  });
-});
-app.listen(PORT, (error) => {
-  if (error) return console.log(error);
-  console.log(`Server running on Port: ${PORT}`);
-});*/
